7b3c68b679d386f32fcd4cb8cd4b719530319b12
Grant Hernandez <grant.h.hernandez@gmail.com>
2017-08-30 11:30:29 -0400

    Getting ready for pypi push

--- setup.py
+++ setup.py
@@ -14,9 +14,8 @@ if sys.platform == 'darwin':
 else:
     library_file = "libpy8051.so"
 
-def _build_pyvex():
+def _build_8051():
     e = os.environ.copy()
-    #e['VEX_PATH'] = '../' + VEX_PATH
     if subprocess.call(['make'], cwd='py8051_c', env=e) != 0:
         raise LibError("Unable to build py8051-static.")
 
@@ -26,15 +25,10 @@ def _shuffle_files():
 
     shutil.copy(os.path.join('py8051_c', library_file), 'py8051/lib')
 
-def _build_ffi():
-    import make_ffi
-    make_ffi.doit(os.path.join(VEX_PATH,'pub'))
-
 class build(_build):
     def run(self):
-        self.execute(_build_pyvex, (), msg="Building pyvex-static")
+        self.execute(_build_8051, (), msg="Building py8051-static")
         self.execute(_shuffle_files, (), msg="Copying libraries and headers")
-        #self.execute(_build_ffi, (), msg="Creating CFFI defs file")
         _build.run(self)
 cmdclass = { 'build': build }
 
@@ -48,17 +42,21 @@ try:
     from setuptools.command.develop import develop as _develop
     class develop(_develop):
         def run(self):
-            #self.execute(_build_vex, (), msg="Building libVEX")
-            self.execute(_build_pyvex, (), msg="Building pyvex-static")
+            self.execute(_build_8051, (), msg="Building py8051-static")
             self.execute(_shuffle_files, (), msg="Copying libraries and headers")
-            #self.execute(_build_ffi, (), msg="Creating CFFI defs file")
             _develop.run(self)
     cmdclass['develop'] = develop
 except ImportError:
     print "Proper 'develop' support unavailable."
 
 setup(
-    name="py8051", version='0.0.0.1', description="A Python interface to an 8051 disassembler.",
+    name="py8051", version='0.1', description="A Python interface to an 8051 disassembler.",
+    keywords=["disassembler", "8051"]
+    author = 'Grant Hernandez',
+    author_email = 'grant.h.hernandez@gmail.com',
+    url = 'https://github.com/grant-h/py8051',
+    download_url = 'https://github.com/grant-h/py8051/archive/0.1.tar.gz',
+
     packages=['py8051'],
     cmdclass=cmdclass,
     install_requires=[ 'pycparser', 'cffi>=1.0.3'],
